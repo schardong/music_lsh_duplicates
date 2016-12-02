d6700d885e2530ab3f761fa81102df5afc1d667d
William Paulo Ducca Fernandes <williamducfer@gmail.com>
2016-12-02 12:22:40 -0200

    - Updates the script that generates the matches dictionary   and the counter of true matches.

--- generate_count_true_and_matches.py
+++ generate_count_true_and_matches.py
@@ -4,6 +4,8 @@
 
 import sys
 import pickle
+import string
+from find_duplicates import is_same_string
 
 
 def usage(scriptname):
@@ -11,20 +13,37 @@ def usage(scriptname):
     exit(1)
 
 
-def check_match(instance1, instance2):
-    # 'instance[12]' is a tuple with the following: 
-    # (website_name, artist_name, lyrics_name, lyrics)
+def check_match(key1, key2, lyrics1, lyrics2):
+    # 'key[12]' is a string with the following: 
+    # 'website_name|artist_name|lyrics_name'
     
-    # Checks whether artist name is the same
-    if not is_same_string(instance1[1], instance2[1], 1):
-        return False
+    key1_split = key1.split('|')
+    key2_split = key2.split('|')
     
-    # Checks whether lyrics name is the same
-    if not is_same_string(instance1[2], instance2[2], 1):
-        return False
+    assert len(key1_split) == 3
+    assert len(key2_split) == 3
     
-    # Checks whether lyrics is the same
-    if not is_same_string(instance1[3], instance2[3], 0.1):
+    try:
+        # Checks whether artist name is the same
+        is_same_artist_name, _ = is_same_string(key1_split[1], key2_split[1], 1)
+        if not is_same_artist_name:
+            return False
+        
+        # Checks whether lyrics name is the same
+        is_same_lyrics_name, _ = is_same_string(key1_split[2], key2_split[2], 1)
+        if not is_same_lyrics_name:
+            return False
+        
+        # Checks whether one of the lyrics is empty
+        if lyrics1 == "" or lyrics2 == "":
+            return False
+        
+        # Checks whether lyrics is the same
+        is_same_lyrics, _ = is_same_string(lyrics1, lyrics2, 0.1)
+        if not is_same_lyrics:
+            return False
+    except Exception as e:
+        print("Error comparing '%s' and '%s'. Returning False for matching." % (key1, key2))
         return False
     
     return True
@@ -35,25 +54,24 @@ def generate_count_true_and_matches(pickle_processed_dict_filename):
     matches = {}
     
     with open(pickle_processed_dict_filename, "rb") as pickle_processed_dict_file:
-        dataset = pickle.load(pickle_processed_dict_file)
+        dict_lyrics = pickle.load(pickle_processed_dict_file)
         
-        # 'instance[12]' is a tuple with the following: 
-        # (website_name, artist_name, lyrics_name, lyrics)
-        for instance1 in dataset:
-            instance1_key = '%s|%s|%s' % (instance1[0], instance1[1], instance1[2])
-            
-            for instance2 in dataset:
-                instance2_key = '%s|%s|%s' % (instance2[0], instance2[1], instance2[2])
+        for dict_lyrics_key1 in dict_lyrics:
+            for dict_lyrics_key2 in dict_lyrics:
+                if dict_lyrics_key1 == dict_lyrics_key2:
+                    continue
                 
-                if (instance1_key, instance2_key) not in matches and \
-                   (instance2_key, instance1_key) not in matches:
-                    is_a_match = check_match(instance1, instance2)
+                if (dict_lyrics_key1, dict_lyrics_key2) not in matches:
+                    is_a_match = check_match(dict_lyrics_key1, dict_lyrics_key2, \
+                                             dict_lyrics[dict_lyrics_key1], dict_lyrics[dict_lyrics_key2])
                     
                     if is_a_match:
-                        matches[(instance1_key, instance2_key)] = True
+                        matches[(dict_lyrics_key1, dict_lyrics_key2)] = True
+                        matches[(dict_lyrics_key2, dict_lyrics_key1)] = True
                         count_true += 1
                     else:
-                        matches[(instance1_key, instance2_key)] = False
+                        matches[(dict_lyrics_key1, dict_lyrics_key2)] = False
+                        matches[(dict_lyrics_key2, dict_lyrics_key1)] = False
     
     return count_true, matches
 
